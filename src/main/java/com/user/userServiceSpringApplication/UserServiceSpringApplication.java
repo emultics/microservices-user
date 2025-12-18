f2f60cb9d9973b022c5f718e99c3b2bfb85f93dc
Suman Manna <sumanmanna@Sumans-MacBook-Air-2.local>
2025-12-18 12:03:44 +0530

    add timezone UTC

--- src/main/java/com/user/userServiceSpringApplication/UserServiceSpringApplication.java
+++ src/main/java/com/user/userServiceSpringApplication/UserServiceSpringApplication.java
@@ -5,10 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.TimeZone;
+
 @EnableCaching
 @EnableTransactionManagement
 @SpringBootApplication
 public class UserServiceSpringApplication {
+
+    static {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceSpringApplication.class, args);
